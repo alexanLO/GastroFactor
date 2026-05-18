fb444a3233bc726e0d31a0becb7de0deab3cab26
alexan lima <alexanlima1.0@gmail.com>
2026-05-18 20:14:25 -0300

    refactor: renomenaod atributos e metodos e imports

--- src/app/pages/main-screen/main-screen.ts
+++ src/app/pages/main-screen/main-screen.ts
@@ -1,16 +1,17 @@
-
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CalculationService } from '../../core/services/calculation.service';
 import { CalculationRequest, CalculationResponse } from '../../shared/models/calculation.model';
 import { CalcularDialog } from '../../component/calcular-dialog/calcular-dialog';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 import { Footer } from '../../component/footer/footer';
+import { NGXLogger } from 'ngx-logger';
+import { RegisterPageComponent } from '../../features/auth/pages/register/register-page.component';
 
 @Component({
   standalone: true,
   selector: 'app-main-screen',
-  imports: [FormsModule, CalcularDialog, Footer, RouterLink],
+  imports: [FormsModule, CalcularDialog, Footer, RouterLink, RegisterPageComponent],
   templateUrl: './main-screen.html',
   styleUrls: ['./main-screen.scss'],
 })
@@ -19,15 +20,16 @@ export class MainScreen {
   foodWeight: number = 0;
   typeWeight: string = 'bruto';
   calculationResult: CalculationResponse | null = null;
-  showDialog: boolean = false;
+  showResultModal: boolean = false;
 
   constructor(
     private calculationService: CalculationService,
     private cdr: ChangeDetectorRef,
+    private log: NGXLogger,
   ) {}
 
   onCalculate() {
-    console.log(
+    this.log.info(
       'Calculando  com o nome do alimento = {}, peso do alimento = {}, tipo de peso = {}',
       this.foodName,
       this.foodWeight,
@@ -42,19 +44,19 @@ export class MainScreen {
       typeWeight: this.mapTypeWeight(this.typeWeight),
     };
 
-    console.log('Chamando requisição do serviço:', request);
+    this.log.info('Chamando requisição do serviço:', request);
 
     this.calculationService.calculateFactor(request).subscribe({
       next: (response) => {
-        console.log('Resposta recebida:', response);
+        this.log.info('Resposta recebida:', response);
         this.calculationResult = response;
-        this.showDialog = true;
+        this.showResultModal = true;
         document.body.classList.add('modal-open'); // trava o scroll
         this.cdr.detectChanges();
       },
       error: (error) => {
-        console.error('Erro no cálculo:', error);
-        this.showDialog = true;
+        this.log.error('Erro no cálculo:', error);
+        this.showResultModal = true;
         document.body.classList.remove('modal-open'); // garante liberar o scroll mesmo em caso de erro
         this.cdr.detectChanges();
       },
@@ -74,8 +76,8 @@ export class MainScreen {
     }
   }
 
-  closeDialog() {
-    this.showDialog = false;
+  closeModal() {
+    this.showResultModal = false;
     document.body.classList.remove('modal-open'); // libera o scroll
   }
 }
