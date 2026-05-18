f57decebeaf3c3e307d72b61c25125c770a0f2fb
alexan lima <alexanlima1.0@gmail.com>
2026-05-18 19:12:10 -0300

    refactor: reestruturando arquivos e pasta do login

---
+++ src/app/features/auth/pages/login/login-page.component/login-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Footer } from '../../../../../component/footer/footer';
+import { NavbarAlternative } from '../../../../../component/navbar-alternative/navbar-alternative';
+
+@Component({
+  selector: 'app-login-page.component',
+  imports: [NavbarAlternative, Footer],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss',
+})
+export class LoginPageComponent {
+ hide = true;
+}
