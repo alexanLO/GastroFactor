cb8f471a5ff3c1f8d7bb4552e32361b386501777
alexan lima <alexanlima1.0@gmail.com>
2026-06-20 23:36:24 -0300

    feat: Criando o componente do metodo de preparação

---
+++ src/app/component/preparation-method-component/preparation-method-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preparation-method-component',
+  imports: [],
+  templateUrl: './preparation-method-component.html',
+  styleUrl: './preparation-method-component.scss',
+})
+export class PreparationMethodComponent {
+
+}
