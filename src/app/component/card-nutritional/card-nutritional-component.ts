6f5466426f604c5f4c0dea53201b7b3bff615a34
alexan lima <alexanlima1.0@gmail.com>
2026-06-20 23:35:49 -0300

    feat: Criando componente do card nutricional

---
+++ src/app/component/card-nutritional/card-nutritional-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-nutritional-component',
+  imports: [],
+  templateUrl: './card-nutritional-component.html',
+  styleUrl: './card-nutritional-component.scss',
+})
+export class CardNutritionalComponent {
+
+}
