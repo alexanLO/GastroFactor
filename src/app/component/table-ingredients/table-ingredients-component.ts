37fdfbe4c40acae20917c8642769cebbb8b3719c
alexan lima <alexanlima1.0@gmail.com>
2026-06-21 01:13:26 -0300

    feat: - Criando logica de adicionar um passo no metodo de preparo

--- src/app/component/table-ingredients/table-ingredients-component.ts
+++ src/app/component/table-ingredients/table-ingredients-component.ts
@@ -11,16 +11,56 @@ import { FormsModule } from '@angular/forms';
 })
 export class TableIngredientsComponent {
   ingredients = [
-    { name: '', netWeight: '', correctionFactor: '', grossWeight: '', cookingFactor: '', totalQuantity: '' },
-    { name: '', netWeight: '', correctionFactor: '', grossWeight: '', cookingFactor: '', totalQuantity: '' },
-    { name: '', netWeight: '', correctionFactor: '', grossWeight: '', cookingFactor: '', totalQuantity: '' }
+    {
+      id: '01',
+      name: '',
+      netWeight: '',
+      correctionFactor: '',
+      grossWeight: '',
+      cookingFactor: '',
+      totalQuantity: '',
+    },
+    {
+      id: '02',
+      name: '',
+      netWeight: '',
+      correctionFactor: '',
+      grossWeight: '',
+      cookingFactor: '',
+      totalQuantity: '',
+    },
+    {
+      id: '02',
+      name: '',
+      netWeight: '',
+      correctionFactor: '',
+      grossWeight: '',
+      cookingFactor: '',
+      totalQuantity: '',
+    }
   ];
 
   addIngredient() {
-    this.ingredients.push({ name: '', netWeight: '', correctionFactor: '', grossWeight: '', cookingFactor: '', totalQuantity: '' });
+    const nextId = (this.ingredients.length + 1).toString().padStart(2, '0');
+    this.ingredients.push({
+      id: nextId,
+      name: '',
+      netWeight: '',
+      correctionFactor: '',
+      grossWeight: '',
+      cookingFactor: '',
+      totalQuantity: '',
+    });
   }
 
   removeIngredient(index: number) {
     this.ingredients.splice(index, 1);
+    this.reindexIngredients();
+  }
+
+  private reindexIngredients() {
+    this.ingredients.forEach((ingredient, i) => {
+      ingredient.id = (i + 1).toString().padStart(2, '0');
+    });
   }
 }
