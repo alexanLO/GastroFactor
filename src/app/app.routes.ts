a79be6bb07c0de4d3ba535c2903a76c6c506f73c
alexan lima <alexanlima1.0@gmail.com>
2026-06-21 08:48:36 -0300

    feat: criando a rota da ficha tecnica e adicionando transição de paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainScreen } from './pages/main-screen/main-screen';
 import { MyCollection } from './pages/my-collection/my-collection';
+import { TechnicalSpecification } from './pages/technical-specification/technical-specification';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'gastrofactor', pathMatch: 'full' },
   { path: 'gastrofactor', component: MainScreen },
   { path: 'meu-acervo', component: MyCollection },
-  { path: 'sobre', component: MainScreen }, // TODO depois criar a pagina sobre
+  { path: 'sobre', component: TechnicalSpecification }, // TODO depois criar a pagina sobre
+  { path: 'ficha-tecnica', component: TechnicalSpecification }
 ];
--- src/app/pages/my-collection/my-collection.ts
+++ src/app/pages/my-collection/my-collection.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 import { FooterComponent } from '../../component/footer/footer.component';
 import { NavbarComponent } from '../../component/navbar/navbar.component';
 
 @Component({
   selector: 'app-my-collection',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent],
+  imports: [FooterComponent, NavbarComponent, RouterLink],
   templateUrl: './my-collection.html',
   styleUrls: ['./my-collection.scss'],
 })
