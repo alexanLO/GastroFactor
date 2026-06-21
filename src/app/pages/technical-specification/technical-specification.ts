fc5de196c12f260aae15969df16d0cb15a834f45
alexan lima <alexanlima1.0@gmail.com>
2026-06-21 18:00:10 -0300

    feat: criando exportação para baixa pdf da ficha tecnica e salvar a ficha no backend

--- src/app/pages/technical-specification/technical-specification.ts
+++ src/app/pages/technical-specification/technical-specification.ts
@@ -1,17 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from '../../component/navbar/navbar.component';
 import { FooterComponent } from '../../component/footer/footer.component';
 import { TableIngredientsComponent } from '../../component/table-ingredients/table-ingredients-component';
 import { CardNutritionalComponent } from '../../component/card-nutritional/card-nutritional-component';
 import { PreparationMethodComponent } from '../../component/preparation-method-component/preparation-method-component';
 import { CardDetailsComponent } from "../../component/card-details-component/card-details-component";
+import { RecipeService, RecipeData } from '../../core/services/recipe.service';
+import { PdfExportService } from '../../core/services/pdf-export.service';
 
 @Component({
   selector: 'app-technical-specification',
   standalone: true,
   imports: [
     CommonModule,
+    HttpClientModule,
     NavbarComponent,
     FooterComponent,
     TableIngredientsComponent,
@@ -24,4 +28,45 @@ import { CardDetailsComponent } from "../../component/card-details-component/car
 })
 export class TechnicalSpecification {
   @Input() isModal: boolean = false;
+
+  @ViewChild(CardDetailsComponent) detailsComponent!: CardDetailsComponent;
+  @ViewChild(TableIngredientsComponent) ingredientsComponent!: TableIngredientsComponent;
+  @ViewChild(CardNutritionalComponent) nutritionalComponent!: CardNutritionalComponent;
+  @ViewChild(PreparationMethodComponent) preparationComponent!: PreparationMethodComponent;
+
+  constructor(
+    private recipeService: RecipeService,
+    private pdfService: PdfExportService
+  ) { }
+
+  saveRecipe(): void {
+    const recipeData: RecipeData = {
+      details: this.detailsComponent.getDetails(),
+      ingredients: this.ingredientsComponent.ingredients,
+      nutritional: this.nutritionalComponent.getNutritional(),
+      preparationMethod: this.preparationComponent.steps
+    };
+
+    this.recipeService.saveRecipe(recipeData).subscribe({
+      next: (response) => {
+        alert('Receita salva com sucesso!');
+        console.log('Receita salva:', response);
+      },
+      error: (error) => {
+        alert('Erro ao salvar a receita!');
+        console.error('Erro:', error);
+      }
+    });
+  }
+
+  exportPdf(): void {
+    const recipeData: RecipeData = {
+      details: this.detailsComponent.getDetails(),
+      ingredients: this.ingredientsComponent.ingredients,
+      nutritional: this.nutritionalComponent.getNutritional(),
+      preparationMethod: this.preparationComponent.steps
+    };
+
+    this.pdfService.generateRecipePdf(recipeData);
+  }
 }
