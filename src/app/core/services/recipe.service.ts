9be79b77fea90d5aaa308eb3fd523b2c43cc8845
Alexan Lima <90114869+alexanLO@users.noreply.github.com>
2026-06-27 18:14:04 -0300

    Develop (#8)
    * Feature/desen atividas (#7)
    
    * refactor: renomeando nome da variavel de font
    
    * feat criando pagina de login component
    
    * feat: update projeto
    
    * feat: criando modal de registrar usuario, adicionando imagens da pagina
    
    * feat:
    - Criando intercptor de erros que vem da api
    - Criando serviço da chamada do endpoit de autenticações do usuario
    - Adicionando NGX Logger
    
    * feat: removendo dependencias antigas
    
    * feat: fazendo comunicação com a api para cadastrar usuario
    
    * feat: criando authguard para proteger acesso de paginas que precisa esta logado
    
    * feat: criando pagina de login
    
    * refactor: ajuste nos links de login
    
    * refactor: refazendo tela de registro
    
    * refactor: correção onde o mouse mudava quando colocava em cima do logo.
    
    * feat: adicionando reset scss global
    
    * feat: criando componente de erro de campos
    
    * feat:Adicionando campos de erro nos forms
    
    * feat: adicionando logica de logout simples
    
    * feat: criando component do footer reutilizavel
    
    * feat: criando navbar reutilizavel
    
    * feat: criando sessao do meu acervo
    
    * feat: adicionando botao de cadastrar usuario no navbar
    
    * refactor: modificando a cor do navbar.
    
    * refactor: remoção de scss que não estao sendo ultilizados.
    
    * feat: Criando o componente de detalhes da ficha tecnica.
    
    * feat: Criando componente do card nutricional
    
    * feat: Criando o componente do metodo de preparação
    
    * feat: Criando o componente da tabela de alimentos da ficha tecnica
    
    * feat: Criando a tela da ficha tecnica
    
    * feat: Ajuste no reset de estilo do input global
    
    * refactor: ajuste nas variaveis scss
    
    * refactor: ajuste no nome de porções para percapita
    
    * feat:
    - Criando logica de adicionar um passo no metodo de preparo
    
    * feat: criando a rota da ficha tecnica e adicionando transição de paginas
    
    * feat: Tranformando a pagina de criar uma ficha tecnica em um modal
    
    * feat: criando exportação para baixa pdf da ficha tecnica e salvar a ficha no backend
    
    * feat: Criando documentação do projeto com criações até a data 21/06/2026
    
    * feat: atualizando dependencias com vulnerabilidades
    
    * refactor: ajuste tecnicos
    
    * feat: atualizando dependencias
    
    * refactor: correção do png
    
    * refactor: ajuste na chamada backend do salvamento de receitas
    
    * feat: ajuste na url da chamada backend

--- src/app/core/services/recipe.service.ts
+++ src/app/core/services/recipe.service.ts
@@ -1,48 +1,35 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-export interface RecipeData {
-  details: {
-    name: string;
-    servings: number;
-    category: string;
-  };
-  ingredients: Array<{
-    name: string;
-    netWeight: string;
-    correctionFactor: string;
-    grossWeight: string;
-    cookingFactor: string;
-    totalQuantity: string;
-  }>;
-  nutritional: {
-    calories: string;
-    protein: string;
-    totalFat: string;
-    carbs: string;
-  };
-  preparationMethod: Array<{
-    id: string;
-    title: string;
-    description: string;
-  }>;
-}
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { environment } from '../../../env/environment';
+import { RecipeData } from '../../shared/models/recipe-data.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
-  private apiUrl = '/api/recipes'; // Ajuste conforme sua API
+  private apiUrl = `${environment.baseAddress}/v1/recipes`;
+  private http = inject(HttpClient);
+  private recipesSubject = new BehaviorSubject<RecipeData[]>([]);
+  recipes$ = this.recipesSubject.asObservable();
 
-  constructor(private http: HttpClient) { }
-
-  saveRecipe(recipe: RecipeData): Observable<any> {
-    return this.http.post(`${this.apiUrl}`, recipe);
+  saveRecipe(recipe: RecipeData): Observable<RecipeData> {
+    return this.http.post<RecipeData>(this.apiUrl, recipe).pipe(
+      tap((newRecipe) => {
+        const current = this.recipesSubject.value;
+        this.recipesSubject.next([...current, newRecipe]);
+      }),
+    );
   }
 
   getRecipe(id: string): Observable<RecipeData> {
     return this.http.get<RecipeData>(`${this.apiUrl}/${id}`);
+  }
+
+  getAllRecipes(): Observable<RecipeData[]> {
+    return this.http
+      .get<RecipeData[]>(this.apiUrl)
+      .pipe(tap((data) => this.recipesSubject.next(data)));
   }
 
   updateRecipe(id: string, recipe: RecipeData): Observable<any> {
--- src/app/pages/technical-specification/technical-specification.ts
+++ src/app/pages/technical-specification/technical-specification.ts
@@ -1,14 +1,14 @@
-import { Component, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NavbarComponent } from '../../component/navbar/navbar.component';
-import { FooterComponent } from '../../component/footer/footer.component';
-import { TableIngredientsComponent } from '../../component/table-ingredients/table-ingredients-component';
+import { Component, inject, Input, ViewChild } from '@angular/core';
+import { CardDetailsComponent } from '../../component/card-details-component/card-details-component';
 import { CardNutritionalComponent } from '../../component/card-nutritional/card-nutritional-component';
 import { PreparationMethodComponent } from '../../component/preparation-method-component/preparation-method-component';
-import { CardDetailsComponent } from "../../component/card-details-component/card-details-component";
-import { RecipeService, RecipeData } from '../../core/services/recipe.service';
+import { TableIngredientsComponent } from '../../component/table-ingredients/table-ingredients-component';
 import { PdfExportService } from '../../core/services/pdf-export.service';
+import { RecipeService } from '../../core/services/recipe.service';
+import { RecipeData } from '../../shared/models/recipe-data.model';
+import { MyCollection } from '../my-collection/my-collection';
 
 @Component({
   selector: 'app-technical-specification',
@@ -16,13 +16,11 @@ import { PdfExportService } from '../../core/services/pdf-export.service';
   imports: [
     CommonModule,
     HttpClientModule,
-    NavbarComponent,
-    FooterComponent,
     TableIngredientsComponent,
     CardNutritionalComponent,
     PreparationMethodComponent,
-    CardDetailsComponent
-],
+    CardDetailsComponent,
+  ],
   templateUrl: './technical-specification.html',
   styleUrl: './technical-specification.scss',
 })
@@ -34,28 +32,27 @@ export class TechnicalSpecification {
   @ViewChild(CardNutritionalComponent) nutritionalComponent!: CardNutritionalComponent;
   @ViewChild(PreparationMethodComponent) preparationComponent!: PreparationMethodComponent;
 
-  constructor(
-    private recipeService: RecipeService,
-    private pdfService: PdfExportService
-  ) { }
+  private collection = inject(MyCollection);
+  private recipeService = inject(RecipeService);
+  private pdfService = inject(PdfExportService);
 
   saveRecipe(): void {
     const recipeData: RecipeData = {
       details: this.detailsComponent.getDetails(),
       ingredients: this.ingredientsComponent.ingredients,
       nutritional: this.nutritionalComponent.getNutritional(),
-      preparationMethod: this.preparationComponent.steps
+      preparationMethod: this.preparationComponent.steps,
     };
 
     this.recipeService.saveRecipe(recipeData).subscribe({
-      next: (response) => {
+      next: () => {
         alert('Receita salva com sucesso!');
-        console.log('Receita salva:', response);
+        this.collection.closeModal();
       },
       error: (error) => {
         alert('Erro ao salvar a receita!');
         console.error('Erro:', error);
-      }
+      },
     });
   }
 
@@ -64,7 +61,7 @@ export class TechnicalSpecification {
       details: this.detailsComponent.getDetails(),
       ingredients: this.ingredientsComponent.ingredients,
       nutritional: this.nutritionalComponent.getNutritional(),
-      preparationMethod: this.preparationComponent.steps
+      preparationMethod: this.preparationComponent.steps,
     };
 
     this.pdfService.generateRecipePdf(recipeData);
