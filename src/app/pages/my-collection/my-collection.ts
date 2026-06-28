61ca11290d58ec81b40f2e0a22bd1bcd6fa69b73
zAlle <alexanlima1.0@gmail.com>
2026-06-28 18:42:38 -0300

    refactor: modificando card para ser onde mostra a data e a hora local

--- src/app/pages/my-collection/my-collection.ts
+++ src/app/pages/my-collection/my-collection.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FooterComponent } from '../../component/footer/footer.component';
 import { NavbarComponent } from '../../component/navbar/navbar.component';
 import { RecipeService } from '../../core/services/recipe.service';
@@ -23,9 +23,27 @@ import { RecipeCardComponent } from '../../component/recipe-card-component/recip
 export class MyCollection implements OnInit {
   recipeService = inject(RecipeService);
   isModalOpen = false;
+  dateTime: Date = new Date();
+
+  horaAtual = signal<Date>(new Date());
+  private intervaloId: any;
+
+  constructor() {
+    // 2. Inicie um intervalo para atualizar o signal a cada 1000ms (1 segundo)
+    this.intervaloId = setInterval(() => {
+      this.horaAtual.set(new Date()); // Atualiza o valor, disparando a mudança na tela
+    }, 1000);
+  }
 
   ngOnInit(): void {
     this.recipeService.refreshRecipes();
+  }
+
+  // 3. Limpeza importante: pare o intervalo quando o componente for destruído
+  ngOnDestroy() {
+    if (this.intervaloId) {
+      clearInterval(this.intervaloId);
+    }
   }
 
   openModal(): void {
