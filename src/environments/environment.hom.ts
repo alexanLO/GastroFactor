e7f41f088b8cfc5836f336b2a31dbe9287cef029
Alexan <alexan.oliveira@bradesco.com.br>
2026-06-29 14:40:45 -0300

    feat: Remover ou ajustar configuracao homologation em angular.json para nao apontar para src/environments/environment.hom.ts inexistente.

---
+++ src/environments/environment.hom.ts
@@ -0,0 +1,6 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --configuration homologation` replaces `environment.ts` with this file.
+export const environment = {
+  production: false,
+  baseAddress: '',
+};
